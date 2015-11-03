db87e8211f2af52e264b8261178c972cd0dfc6b5
Dino Prašo <desmond1303@gmail.com>
2015-11-03 10:02:42 +0100

    Realm (and small fixes)

--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -87,6 +87,8 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/Alamofire.framework"
   install_framework "Pods/AlamofireObjectMapper.framework"
   install_framework "Pods/ObjectMapper.framework"
+  install_framework "Pods/Realm.framework"
+  install_framework "Pods/RealmSwift.framework"
   install_framework "Pods/SDWebImage.framework"
   install_framework "Pods/SwiftyJSON.framework"
 fi
@@ -94,6 +96,8 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/Alamofire.framework"
   install_framework "Pods/AlamofireObjectMapper.framework"
   install_framework "Pods/ObjectMapper.framework"
+  install_framework "Pods/Realm.framework"
+  install_framework "Pods/RealmSwift.framework"
   install_framework "Pods/SDWebImage.framework"
   install_framework "Pods/SwiftyJSON.framework"
 fi
---
+++ newIMDbv2/TMDRLMMovies.swift
@@ -0,0 +1,25 @@
+//
+//  TMDRLMMovies.swift
+//  newIMDbv2
+//
+//  Created by Dino Praso on 3.11.15.
+//  Copyright © 2015 Atlantbh. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class TMDRLMMovies: Object {
+    dynamic var id = 0
+    dynamic var title : String = ""
+    dynamic var originalTitle : String = ""
+    dynamic var imagePath : String = ""
+    dynamic var genres = NSMutableArray()
+    dynamic var movieDescription : String = ""
+    dynamic var releaseDate : String = ""
+    dynamic var popularity : Double = 0
+    dynamic var runtime : Int = 0 //in minutes
+    dynamic var voteAvg : Double = 0
+    dynamic var voteCount : Int = 0
+    
+}
